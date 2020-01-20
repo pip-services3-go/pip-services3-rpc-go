92aeae88885f403a8de19a8598a74d3928ec9f26
Levichev Dmitry <levichev.dmitry@gmail.com>
2020-01-20 12:24:59 +0300

    Delete IRegisterabl.go

--- services/IRegisterabl.go
+++
@@ -1,8 +0,0 @@
-package services
-/*
- IRegisterable is interface to perform on-demand registrations.
- */
-type IRegisterable interface {
-    // Perform required registration steps.
-    Register()
-}
