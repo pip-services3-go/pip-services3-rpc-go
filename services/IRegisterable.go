9c31f91cfc3918c9b089b18eb314b2f51776176f
Levichev Dmitry <levichev.dmitry@gmail.com>
2020-01-20 12:24:22 +0300

    Create IRegisterable.go

---
+++ services/IRegisterable.go
@@ -0,0 +1,8 @@
+package services
+/*
+ IRegisterable is interface to perform on-demand registrations.
+ */
+type IRegisterable interface {
+    // Perform required registration steps.
+    Register()
+}
